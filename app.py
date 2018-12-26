6666b1480d445cb0002b4dacff671e725440eba4
aaronzhang1234 <nimbyest@gmail.com>
2018-12-25 20:45:58 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,79 @@
+from fbchat import log, Client
+from fbchat.models import *
+import psycopg2
+import json
+import time
+
+conn = psycopg2.connect("dbname=messenger user=sp1r3")
+cur = conn.cursor() 
+thread_type = ThreadType.GROUP
+
+class CustomClient(Client):
+    def onPeopleAdded(self, added_ids, author_id, thread_id, **kwargs):
+        current_time = time.clock_gettime(0)
+        for added_id in added_ids:
+            cur.execute("INSERT INTO CaG (UID, THREAD_ID, TIMESTAMP, GOING) values (%s, %s, %s, %s);", (added_id, thread_id, current_time, False))
+            conn.commit()
+            #print(added_id + " got added!")
+    def onPersonRemoved(self, removed_id, author_id, thread_id, **kwargs):
+        current_time = time.clock_gettime(0)
+        #print(removed_id + " left group")
+        cur.execute("INSERT INTO CaG (UID, THREAD_ID, TIMESTAMP, GOING) values (%s, %s, %s, %s);", (removed_id, thread_id, current_time, True))
+        conn.commit()
+    def onMessageSeen(self, seen_by, thread_id, thread_type, seen_ts, ts, metadata, msg):
+        self.addUser(seen_by)
+    def onMessage(self, mid, author_id, message_object, thread_id, thread_type, ts, metadata, msg, **kwargs):
+        self.addUser(author_id)
+        uid = message_object.uid
+        author = message_object.author
+        threadid = metadata['threadKey']['threadFbId']
+        timestamp = int(message_object.timestamp)/1000
+        if len(message_object.attachments)>=1:
+            image_urls = []
+            for image in message_object.attachments:
+                image_urls.append(image.preview_url) 
+            image_urls_text = '\n'.join(image_urls)
+            cur.execute("INSERT INTO messages(messageid, text, author, threadid, timestamp) VALUES (%s, %s, %s, %s, %s);", (uid, image_urls_text, author, threadid, timestamp))
+            conn.commit() 
+        else:
+            text = message_object.text
+            if text=='disco':
+                self.disco_time(threadid)
+            cur.execute("INSERT INTO messages(messageid, text, author, threadid, timestamp) VALUES (%s, %s, %s, %s, %s);", (uid, text, author, threadid, timestamp))
+            conn.commit() 
+            #print("Author: " + message_object.author + '\n' + "Text: " + message_object.text+ '\n' + "TimeStamp: " + str(message_object.timestamp))
+    def addUser(self, uid):    
+        cur.execute("ROLLBACK")
+        cur.execute("SELECT name FROM users WHERE uid = %s;", (uid,))
+        user = cur.fetchone()
+        if not user:
+            #print("adding user")
+            user = self.fetchUserInfo(uid)[uid]
+            cur.execute("INSERT INTO USERS(uid, name, profilepic) VALUES (%s, %s, %s);", (user.uid, user.name, user.photo))
+            conn.commit()
+    def disco_time(self, thread_id):
+        colors = [ThreadColor.BILOBA_FLOWER,
+                  ThreadColor.MESSENGER_BLUE, 
+                  ThreadColor.VIKING,
+                  ThreadColor.GOLDEN_POPPY,
+                  ThreadColor.RADICAL_RED, 
+                  ThreadColor.SHOCKING,
+                  ThreadColor.PICTON_BLUE,
+                  ThreadColor.FREE_SPEECH_GREEN,
+                  ThreadColor.PUMPKIN,
+                  ThreadColor.LIGHT_CORAL,
+                  ThreadColor.MEDIUM_SLATE_BLUE,
+                  ThreadColor.DEEP_SKY_BLUE,
+                  ThreadColor.FERN, 
+                  ThreadColor.CAMEO, 
+                  ThreadColor.BRILLIANT_ROSE]
+        for color in colors:
+            self.changeThreadColor(color, thread_id)            
+
+conf = json.load(open('config.json', 'r'))
+client = CustomClient(conf['username'],conf['password'])
+client.listen()
+cur.close()
+conn.close()
+#messages = client.fetchThreadMessages(thread_id = thread_id, limit=200)
+#client.send(Message(text='fug'), thread_id=thread_id, thread_type=thread_type)
---
+++ graphmaker.py
@@ -0,0 +1,29 @@
+from datetime import datetime, time
+import time as t
+import psycopg2
+import json
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+conf = json.load(open('config.json', 'r'))
+
+conn = psycopg2.connect("dbname=messenger user=sp1r3")
+cur = conn.cursor()
+
+cur.execute('SELECT NAME, COUNT(NAME) FROM MESSAGES RIGHT JOIN USERS ON author=uid WHERE THREADID=%s GROUP BY NAME', (conf["thread_main"],));
+messagesent = cur.fetchall()
+names = []
+numsent = []
+for msg in messagesent:
+    names.append(msg[0])
+    numsent.append(msg[1])
+#    msgtime = t.strftime('%Y-%m-%d %H:%M:%S', t.localtime(int(ts[0])))
+
+midnight = datetime.combine(datetime.today(), time.hour(8))
+
+print(midnight)
+fig1, ax1 = plt.subplots()
+ax1.pie(numsent, labels = names, shadow=True, startangle=90)
+ax1.axis('equal')
+plt.show()
