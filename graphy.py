354d34c4d0599473660822f905294d25310fe2e5
aaronzhang1234 <nimbyest@gmail.com>
2019-01-09 15:57:08 -0500

    added sessions

--- graphy.py
+++ graphy.py
@@ -7,6 +7,12 @@ import os
 thread_type = ThreadType.GROUP
 
 os.system('rm photos/*.png')
+session_cookies = {}
+try:
+    with open('creds/session.json','r') as f:
+        session_cookies = json.load(f)
+except:
+    pass
 grapherson = Grapher()
 #print(grapherson.flesch("week"))
 #grapherson.graph_of_messages(timespan="day")
@@ -14,3 +20,5 @@ grapherson.wordcloud(True, timespan = "", show_names = True)
 #conf = json.load(open('creds/config.json', 'r'))
 #client = Client(conf['username_kino'],conf['password_kino'], session_cookies = session_cookies)
 #client.sendLocalImage('photos/today.png', message= Message(text='oef'), thread_id=conf['thread_bobby'], thread_type=thread_type)
+with open('session.json','w') as f:
+    json.dump(client.getSession(), f)
